1811516d95440129fa8f0471635af5f61e89f3f1
sysadmin <virajv2005@gmail.com>
2024-08-21 14:55:52 +0530

    New Test files added

---
+++ factorial.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+long long factorial(int n)
+{
+if(n == 0)
+return 1;
+else
+return n * factorial(n-1);
+}
+
+int main()
+{
+int n;
+cin >> n;
+cout << factorial(n) << endl;
+}
